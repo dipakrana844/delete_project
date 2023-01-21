a7deaa32c03fec6c55f0c11908c2c0a0f6dba2dc
RanaDipak <dipakrana2001@gmail.com>
2023-01-21 18:26:02 +0530

    dailog page added

---
+++ lib/dailog.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dailog extends StatefulWidget {
+  const Dailog({Key? key}) : super(key: key);
+
+  @override
+  State<Dailog> createState() => _DailogState();
+}
+
+class _DailogState extends State<Dailog> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
