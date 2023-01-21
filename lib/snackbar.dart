8c22762973f4f52527ed2b277856eb93ac0c4e62
RanaDipak <dipakrana2001@gmail.com>
2023-01-21 18:33:22 +0530

    dailog page added

---
+++ lib/snackbar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Snackbar extends StatefulWidget {
+  const Snackbar({Key? key}) : super(key: key);
+
+  @override
+  State<Snackbar> createState() => _SnackbarState();
+}
+
+class _SnackbarState extends State<Snackbar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
